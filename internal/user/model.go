d3982671df8ff7410dfa92fdd2e6fa39a42a939a
Victoria Kashurkina <kvictorijas@gmail.com>
2026-01-28 11:25:45 +0300

    Updated architecture: added profile, stats and settings

--- internal/user/model.go
+++ internal/user/model.go
@@ -1,10 +1,12 @@
 package user
 
+import "time"
+
 type User struct {
 	ID			uint	`gorm:"primaryKey"`
 	Email		string	`gorm:"unique;not null"`
 	Password	string	`gorm:"not null"`
 	RoleID		uint
+	CreateAt	time.Time
+	UpdatedAt	time.Time
 }
-
-
