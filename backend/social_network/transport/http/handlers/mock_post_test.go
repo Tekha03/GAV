b258b52395f09b40911317257e8fc5b0d9476099
Tehron Janobilov <tsdzhanobilov@edu.hse.ru>
2026-03-29 11:40:41 +0300

    Add swagger and tests for like handler

--- backend/social_network/transport/http/handlers/mock_post_test.go
+++ backend/social_network/transport/http/handlers/mock_post_test.go
@@ -20,6 +20,11 @@ func (m *MockPostService) Create(ctx context.Context, userID uuid.UUID, content,
 
 func (m *MockPostService) GetByID(ctx context.Context, id uuid.UUID) (*post.Post, error) {
 	args := m.Called(ctx, id)
+
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+
 	return args.Get(0).(*post.Post), args.Error(1)
 }
 
