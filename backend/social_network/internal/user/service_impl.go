93a7632c32a3ce13535b63ce25907b28a5ab1fd0
Tehron Janobilov <tsdzhanobilov@edu.hse.ru>
2026-04-07 10:42:07 +0300

    Fix user service

--- backend/social_network/internal/user/service_impl.go
+++ backend/social_network/internal/user/service_impl.go
@@ -63,14 +63,10 @@ func (s *service) FindDogsNearby(ctx context.Context, userID uuid.UUID, centerLa
 		return nil, err
 	}
 
-	var result []*dog.Dog
+	result := make([]*dog.Dog, 0)
 
 	for _, currentDog := range dogs {
-		if currentDog.OwnerID == userID {
-			continue
-		}
-
-		if currentDog.Visibility == dog.VisibilityFollowersOnly {
+		if currentDog.OwnerID == userID || currentDog.Visibility != dog.VisibilityEveryone {
 			continue
 		}
 
